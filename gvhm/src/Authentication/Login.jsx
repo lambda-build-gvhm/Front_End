675450875963082865924e4810435531cc3d4f3a
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-11 14:15:42 -0700

    Psuedo login implemented while waiting on backend functionality, Design and layout needs to be decided on by theming convention. This is mainly to deploy to a netlify URL for UI/UX to be able to go ahead and link to.

--- gvhm/src/Authentication/Login.jsx
+++ gvhm/src/Authentication/Login.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-
+import './auth.css'
 
 const Login = (props) => {
 
+    // login = (e) =>{
+    //     axios.post('http://localhost:5000/api/login', creds)
+    //     .then(res => {localStorage.setItem('token', res.data.payload)})
+    //     .catch(err => console.log(err))
+    //     .then(() => this.props.history.push('/'))
+    // }
+
     return ( 
-        <>
-        <form onSubmit={() => localStorage.setItem('token', 'bsToken') }>
-          <input
-            type="text"
-            name="username"
-          />
-          <input
-            type="password"
-            name="password"
-          />
-          <button>Log in</button>
-        </form>
-        </>
+        <div className='login'>
+            <h3>Please Enter a username and password</h3>
+            <h4>I don't know why you would need one either.... you can leave it blank lol.</h4>
+            <form onSubmit={(e) => localStorage.setItem('token', 'bsToken') }>
+                <input
+                    type="text"
+                    name="username"
+                />
+                <input
+                    type="password"
+                    name="password"
+                />
+                <button>Log in</button>
+            </form>
+        </div>
      );
 }
  
--- gvhm/src/Components/DataPage.jsx
+++ gvhm/src/Components/DataPage.jsx
@@ -4,6 +4,11 @@ const DataPage = (props) => {
     return ( 
         <div className="mainpage">
             <h1>Gun Violence Heat Map</h1>
+            <iframe src="https://plot.ly/~krsmith/2.embed"         
+                height="600" width="100%"         
+                scrolling="no" seamless="seamless"         
+                frameBorder="0"> 
+            </iframe>
         </div>
      );
 }
