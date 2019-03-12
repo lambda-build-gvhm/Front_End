84099c505f73ea887bfc3c6aacb6a65cfcc45a2d
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-12 14:22:33 -0700

    More styling changes, Back end is now up so will be setting up auth now.

--- gvhm/src/Authentication/Login.jsx
+++ gvhm/src/Authentication/Login.jsx
@@ -1,36 +1,70 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
-
+import React, {useState} from 'react'
+import axios from 'axios'
 import './auth.css'
 
 const Login = (props) => {
+    const [usernameInput, updateUsernameInput, setUsernameText] = useInput()
+    const [passInput, updatePassInput, setPassText] = useInput()
+    const user = {
+        username: usernameInput,
+        password: passInput,
+    }
 
-    // login = (e) =>{
-    //     axios.post('http://localhost:5000/api/login', creds)
-    //     .then(res => {localStorage.setItem('token', res.data.payload)})
-    //     .catch(err => console.log(err))
-    //     .then(() => this.props.history.push('/'))
-    // }
+    const loginRegister = e => {
+        e.preventDefault()
+        // axios.post(`http://localhost:5000/api/${ e.target.className !== 'registration-button' ? 'login' : 'register'}`, user)
+        //     .then(res => {localStorage.setItem('token', res.data.payload); this.props.history.push('/')})
+        //     .catch(err => console.log(err))
+            localStorage.setItem('token', 'bsToken')
+            console.log(e.target)
+            setPassText('');
+            setUsernameText('')
+        }
+
 
     return ( 
         <div className='login'>
             <h3>Please Enter a username and password</h3>
-            <h4>I don't know why you would need one either.... you can leave it blank lol.</h4>
-            <form onSubmit={(e) => localStorage.setItem('token', 'bsToken') }>
+            <h4>No Account?</h4>
+            <h4>No Problem</h4>
+            <h4>Just Submit the Login Creds and We Will Make One For You</h4>
+            <form onSubmit={(e) => loginRegister(e)} >
                 <input
                     type="text"
                     name="username"
+                    value={usernameInput}
+                    onChange={e => updateUsernameInput(e)}
+                    required
                 />
                 <input
                     type="password"
                     name="password"
+                    value={passInput}
+                    onChange={e => updatePassInput(e)}
+                    required
                 />
                 <button>Log in</button>
             </form>
+            <button className='register-button' onClick={()=>{}}>Register</button>
         </div>
      );
 }
- 
+
+const useInput = () =>{
+    const [inputText, setInputText] = useState('')
+
+    const updateInput = (e) => {
+        setInputText(e.target.value)
+    }
+
+    return[
+        inputText,
+        updateInput,
+        setInputText
+    ]
+    
+}
+
 export default Login;
 
 
