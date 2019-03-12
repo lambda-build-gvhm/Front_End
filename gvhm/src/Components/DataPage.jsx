d40bb5ed7b46490d7b7493e8f266b09887030246
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-12 14:34:39 -0700

    Styling changes

--- gvhm/src/Components/DataPage.jsx
+++ gvhm/src/Components/DataPage.jsx
@@ -4,36 +4,36 @@ const DataPage = (props) => {
     return ( 
         <div className="mainpage">
             <h1>Gun Violence Heat Map</h1>
-            {/* <iframe 
+            <div className="flex-container">
+            <iframe 
                 src="https://plot.ly/~krsmith/2.embed"         
                 height="600" width="100%"         
                 scrolling="no" seamless="seamless"         
                 frameBorder="0"> 
-            </iframe> */}
-            <div className="flex-container">
-                <iframe 
-                    width="900" 
-                    height="800" 
-                    frameborder="0" 
-                    scrolling="no" 
-                    src="//plot.ly/~otillieodd/137.embed">
-                </iframe>
-                <iframe 
-                    width="900" 
-                    height="800" 
-                    frameborder="0" 
-                    scrolling="no" 
-                    src="//plot.ly/~otillieodd/160.embed">
-                </iframe>
-            </div>
-            {/* <iframe
+            </iframe>
+            <iframe 
+                width="900" 
+                height="800" 
+                frameborder="0" 
+                scrolling="no" 
+                src="//plot.ly/~otillieodd/137.embed">
+            </iframe>
+            <iframe 
+                width="900" 
+                height="800" 
+                frameborder="0" 
+                scrolling="no" 
+                src="//plot.ly/~otillieodd/160.embed">
+            </iframe>
+            <iframe
                 className="lastgraph" 
                 width="900" 
                 height="800" 
                 frameborder="0" 
                 scrolling="no" 
                 src="//plot.ly/~krsmith/4.embed">
-            </iframe> */}
+            </iframe>
+            </div>
         </div>
      );
 }
