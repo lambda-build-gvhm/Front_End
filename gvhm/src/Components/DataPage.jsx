c6a91a470760f0c667723842da17dd5a792f402a
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-12 14:48:43 -0700

    More styling adjustments

--- gvhm/src/Components/DataPage.jsx
+++ gvhm/src/Components/DataPage.jsx
@@ -7,9 +7,17 @@ const DataPage = (props) => {
             <div className="flex-container">
             <iframe 
                 src="https://plot.ly/~krsmith/2.embed"         
-                height="600" width="100%"         
+                height="600" width="112.999%"         
                 scrolling="no" seamless="seamless"         
                 frameBorder="0"> 
+            </iframe>
+            <iframe
+                className="lastgraph" 
+                width="900" 
+                height="800" 
+                frameborder="0" 
+                scrolling="no" 
+                src="//plot.ly/~krsmith/4.embed">
             </iframe>
             <iframe 
                 width="900" 
@@ -24,14 +32,6 @@ const DataPage = (props) => {
                 frameborder="0" 
                 scrolling="no" 
                 src="//plot.ly/~otillieodd/160.embed">
-            </iframe>
-            <iframe
-                className="lastgraph" 
-                width="900" 
-                height="800" 
-                frameborder="0" 
-                scrolling="no" 
-                src="//plot.ly/~krsmith/4.embed">
             </iframe>
             </div>
         </div>
