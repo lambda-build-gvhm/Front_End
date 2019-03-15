576a1cfeb42e74cc1d39d16856da7170e1086972
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-03-15 02:57:35 -0700

    Added text to guide users through graphics.

--- gvhm/src/Components/DataPage.jsx
+++ gvhm/src/Components/DataPage.jsx
@@ -37,12 +37,16 @@ export default (props) => {
             <div className="flex-container">
 
                 {/*State Tabs Selector*/}
+                <h2>A state choropleth map sectioned by year. California, Texas, Florida, Illinois, and Ohio remain consistently some of the most violent states.</h2>
                 <Tabs tabs={TABS_BY_STATE} />
-
+                <img className='state-compare' src='https://files.slack.com/files-pri/T4JUEB3ME-FGZAJ11MK/comparison.png' alt='State-comparison-bar-graph' />
+                <h2>A county choropleth map sectioned by year. To get more granular detail, click and hold your mouse over an area you wish to zoom in. To zoom out, double click.</h2>
                 {/*County Maps Tab selector */}
                 <Tabs tabs={TABS_BY_COUNTY} />
 
                 {/* Additional Graphics */}
+                {/* Bubble Graphic */}
+                <h2> A density map that focuses on singular incidents of shootings. The more large the circle, the greater the amount of killings in the gun violence incident. You can explore the incidents by panning and zooming in on the areas. You can choose to look from a high level or zoom down to the street level.</h2>
                 <div className="iframecontainer">
                     <iframe
                         src="https://gvheatmap.herokuapp.com/html/incident_severity_map.html"
@@ -53,9 +57,11 @@ export default (props) => {
                         frameBorder="0"
                     />
                 </div>
+                    <h2>  Move the left and right slider at the bottom to select the time frame. The chunk of time you select will show the amount of people both killed and injured by gun violence in that time frame. </h2>
                 <div className="iframecontainer">
                     <iframe src="//plot.ly/~otillieodd/137.embed" height="600" width='100%' frameborder="0" scrolling="no" />
                 </div>
+                    <h2> A simple animation to track gun violence's total incident count in the form of linear change from the start to end of the data source. Please press play to see the animation </h2>
                 <div className="iframecontainer">
                     <iframe height="600" width='100%' frameborder="0" scrolling="no" src="//plot.ly/~otillieodd/160.embed" />
                 </div>
